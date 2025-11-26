9d190159e62642a38910ec89db79d7c49eb60de7
Zurthr <outdatin@gmail.com>
2025-11-27 02:34:18 +0700

    global cssing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,10 +6,9 @@ import { fileURLToPath } from 'node:url'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['~/assets/css/global.css'],
+  css: ['~/assets/global.css'],
   alias: {
     'images': fileURLToPath(new URL('./assets/images', import.meta.url)),
     'style': fileURLToPath(new URL('./assets/style', import.meta.url)),
-    'data': fileURLToPath(new URL('./assets/other/data', import.meta.url)),
   },
 })
