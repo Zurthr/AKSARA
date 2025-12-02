290ee1823149bdcf60132df3feec6a110e5e2a53
alifiofajarnugraha <alifiofajarnugraha@gmail.com>
2025-12-02 21:02:51 +0700

    memperbaiki bug dikit

--- app/pages/Events/index.vue
+++ app/pages/Events/index.vue
@@ -199,7 +199,8 @@ const popularTags = [
   margin-bottom: 16px;
   flex: 1;
   display: -webkit-box;
-  -webkit-line-clamp: 3;
+  -webkit-line-clamp: 2;
+  line-clamp: 2;
   -webkit-box-orient: vertical;
   overflow: hidden;
 }
